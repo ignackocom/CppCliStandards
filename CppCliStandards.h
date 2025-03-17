fcb6d0cb1f74e2f964ec6c99ac5d7dabd9658057
ignackocom <peter.potrok@outlook.sk>
2025-03-17 19:53:27 +0100

    Add files via upload

---
+++ CppCliStandards.h
@@ -0,0 +1,22 @@
+/******************************************************************************
+* @file      CppCliStandard.h
+* @author    Peter Potrok
+* @copyright Copyright (c) 1994 - 2025
+*            MIT License (see License.txt file)
+* @brief     Cpp Standard definitions
+* @details
+*
+* @see       https://en.wikipedia.org/wiki/C%2B%2B/CLI
+*            https://learn.microsoft.com/en-us/cpp/preprocessor/predefined-macros?view=msvc-170
+*            https://github.com/cpredef/predef
+******************************************************************************/
+
+#ifndef CPPCLISTANDARD_H
+#define CPPCLISTANDARD_H		200406L
+
+
+/* official standard ECMA-372 standard */
+#define CPPCLI		200406L
+
+
+#endif /* CPPCLISTANDARD_H */
